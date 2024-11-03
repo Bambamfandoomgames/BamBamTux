d5c5b951d7710639e1ad770634e693a9e015ef10
Gabriel Tech <gabriel@X201.localdomain>
2024-11-03 09:12:26 +0200

    Added more logos...

--- bambam.c
+++ bambam.c
@@ -1,14 +1,34 @@
-#include<stdlib.h>
 #include<time.h>
 #include "raylib.h"
+
 #define NMAX 50
+#define TTL 7
 
 struct fig {
-    Vector2 pos;
-    double time;
-} V[NMAX];
+    int index;//index in the vector of textures
+    Vector2 pos;//position in window
+    double time;//time of creation
+};
 
-int n=0;
+struct fig V[NMAX];//vector of figures
+int nfig=0;//number of figures
+
+Texture2D T[NMAX];//number of textures
+int ntext=0;//number of textures
+
+void CreateTexture(char path[])
+{
+    Image img = LoadImage(path);
+    ImageResize(&img,img.width/2,img.height/2);
+    T[ntext++] = LoadTextureFromImage(img);
+    UnloadImage(img);
+}
+
+void CleanTextures()
+{
+    for(int i=0;i<ntext;i++)
+        UnloadTexture(T[i]);
+}
 
 int main(void)
 {
@@ -19,26 +39,26 @@ int main(void)
 
     InitWindow(screenWidth, screenHeight, "Bam Bam - Linux Logos");
 
-    
-    Image tux = LoadImage("resources/img/Tux.png");
-    ImageResize(&tux,tux.width/2,tux.height/2);
-    Texture2D texture = LoadTextureFromImage(tux);
-    UnloadImage(tux);
+    CreateTexture("resources/img/Tux.png");
+    CreateTexture("resources/img/Pax_tux.png");
+    CreateTexture("resources/img/Debian.png");
+    CreateTexture("resources/img/Archlinux.png");
+    CreateTexture("resources/img/Jellyfish.png");
+
 
     SetTargetFPS(60);
-
-    int x = GetRandomValue(0,screenWidth-texture.width);
-    int y = GetRandomValue(0,screenHeight-texture.height);
 
     while (!WindowShouldClose())
     {
         // Update
-        if(IsKeyPressed(KEY_T) && n<NMAX)
+        if(IsKeyPressed(KEY_T) && nfig<NMAX)
         {
-            V[n].pos.x = GetRandomValue(0,screenWidth-texture.width);
-            V[n].pos.y = GetRandomValue(0,screenHeight-texture.height);
-            V[n].time = GetTime();
-            ++n;
+            int idx = GetRandomValue(0,ntext-1);
+            V[nfig].index = idx;
+            V[nfig].pos.x = GetRandomValue(0,screenWidth-T[idx].width);
+            V[nfig].pos.y = GetRandomValue(0,screenHeight-T[idx].height);
+            V[nfig].time = GetTime();
+            ++nfig;
         }
 
         // Draw
@@ -46,26 +66,26 @@ int main(void)
 
             ClearBackground(WHITE);
 
-            for(int i=0;i<n;i++)
+            for(int i=0;i<nfig;i++)
             {
-                if(GetTime()-V[i].time < 5)
-                    DrawTexture(texture, V[i].pos.x, V[i].pos.y, WHITE);
+                if(GetTime()-V[i].time <= TTL)
+                    DrawTexture(T[V[i].index], V[i].pos.x, V[i].pos.y, WHITE);
                 else
                 {
-                    if(n>1)
+                    if(nfig > 1)
                     {
                     struct fig x = V[i];
-                    V[i] = V[n-1];
-                    V[n-1] = x;
+                    V[i] = V[nfig-1];
+                    V[nfig-1] = x;
                     }
-                    --n;
+                    --nfig;
                 }
             }
 
         EndDrawing();
     }
 
-    UnloadTexture(texture);
+    CleanTextures();
 
     CloseWindow();
 
